6076c5704eb04693cc14cfa05ebd4021e232d537
Brett Terpstra <me@brettterpstra.com>
2024-03-22 10:56:30 -0500

    quote argument

--- functions/cdt.fish
+++ functions/cdt.fish
@@ -1,7 +1,7 @@
 function cdt -d 'Change dir based on TagFiler tags'
 	# assumes cdt.bash (in the bash_scripts folder) is in your path
 	set -l target (bash cdt.bash $argv)
-	if test (string length $target) -gt 0
+	if test (string length "$target") -gt 0
 		cd "$target"
 		return 0
 	else
