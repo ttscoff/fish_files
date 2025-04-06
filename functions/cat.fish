c27a84037d48d353a7f2b36af935257c76ec25b7
Brett Terpstra <brett.terpstra@oracle.com>
2025-04-06 07:00:26 -0500

    better cat command
    @improved cat command now recognizes images and pdfs, using imgcat, differentiates markdown files and uses mdless, and will display binary files using bat -A. Requires the is.fish function.

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,14 +1,35 @@
-function cat -d "Use bat instead of cat unless it's a Markdown file, then use mdless"
-	set -l exts md markdown txt taskpaper
+function cat -d "Use appropriate cat replacement for file type"
+    for file in $argv
+        set -l exts md markdown txt taskpaper mmd mdown mdwn mkdn mkdown
 
-	if not test -f $argv[-1]
-		echo "File not found: "$argv[-1]
-		return 0
-	end
+        if not test -f $file
+            echo "File not found:  $file"
+            continue
+        end
 
-	if contains (get_ext $argv[-1]) $exts
-		mdless $argv
-	else
-		command bat --style plain --theme OneHalfDark $argv
-	end
+        if is image $file || is pdf $file
+            # Use the `imgcat` command to display images in the terminal
+            imgcat $file
+            continue
+        end
+
+        if is docx $file
+            # Use the `docx2txt` command to convert docx files to plain text
+            pandoc -t markdown_mmd -o - --wrap=none --reference-links $file | mdless
+            continue
+        end
+
+        if is text $file
+            if is markown $file || contains (get_ext  $file) $exts
+                mdless $file
+            else
+                command bat --style plain $file
+            end
+            continue
+        end
+
+        bat -A --style plain $files
+    end
+
+    return 0
 end
