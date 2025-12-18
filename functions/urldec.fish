1cc53068b5f6c89b7f27e24571b99f2a3a748cfd
Brett Terpstra <me@brettterpstra.com>
2025-12-18 05:48:35 -0600

    Improve URL encoding/decoding functions
    @improved urldec and urlenc now properly handle multi-line input from stdin
    @improved urldec and urlenc now correctly handle trailing newlines in piped input
    @improved urldec now uses echo -e for proper escape sequence output
    
    Standardized comparison operators to use -gt instead of >
    Fixed indentation consistency in urlenc function
    Removed obsolete comment from urlenc function

--- functions/urldec.fish
+++ functions/urldec.fish
@@ -1,8 +1,15 @@
 function urldec --description 'URL decode'
-    if test (count $argv) >0
-        echo -n "$argv" | perl -pe'$string = s/%([A-Fa-f\d]{2})/chr hex $1/eg;print $string;'
+    if test (count $argv) -gt 0
+        set output (echo -en "$argv" | perl -pe's/%([A-Fa-f\d]{2})/chr hex $1/eg')
+        echo -e "$output"
     else
-        command cat | perl -pe'$string=s/%([A-Fa-f\d]{2})/chr hex $1/eg;print $string;'
+        set input ""
+        while read -l line
+            set input "$input$line\n"
+        end
+        # Remove the trailing newline
+        set input (string trim -r -- "$input")
+        set output (echo -en "$input" | perl -pe's/%([A-Fa-f\d]{2})/chr hex $1/eg')
+        echo -e "$output"
     end
-
 end
--- functions/urlenc.fish
+++ functions/urlenc.fish
@@ -1,8 +1,14 @@
-# Defined in /Users/ttscoff/.config/fish/brett.fish @ line 210
+
 function urlenc --description 'url encode the passed string'
-	if test (count $argv) > 0
-    echo -n "$argv" | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg'
-  else
-    command cat | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg'
-  end
+    if test (count $argv) -gt 0
+        echo -n "$argv" | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg'
+    else
+        set input ""
+        while read -l line
+            set input "$input$line\n"
+        end
+        # Remove the trailing newline
+        set input (string trim -r -- "$input")
+        echo -n "$input" | perl -pe's/([^-_.~A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg'
+    end
 end
