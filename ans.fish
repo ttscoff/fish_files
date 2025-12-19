0715a444d9c5e42ad6d81f04730d34916f4cc38d
Brett Terpstra <me@brettterpstra.com>
2025-12-19 07:11:46 -0600

    expand ans

--- ans.fish
+++ ans.fish
@@ -1,3 +1,24 @@
-function ans --description 'Get the accepted answer from StackOverflow as Markdown'
-    gather --accepted-only (sl "!stackoverflow.com $argv!!")
+function ans --description 'Get the accepted or all answers from StackOverflow as Markdown'
+    argparse h/help a/all -- $argv
+    if set -q _flag_help
+        echo "ans - Get the accepted answer from StackOverflow as Markdown"
+        echo
+        echo "Usage: ans [search terms]"
+        echo
+        echo "Options:"
+        echo "  -h, --help    Show this help message"
+        echo "  -a, --all     Show all answers (default accepted only)"
+        echo
+        echo "Requirements:"
+        echo "  - searchlink gem (install with: `gem install searchlink`)"
+        echo "  - gather CLI (install with: `brew install gather`)"
+        return 0
+    end
+
+    if set -q _flag_all
+        gather (echo "!stackoverflow.com $argv!!" | searchlink)
+    else
+        gather --accepted-only $acc (echo "!stackoverflow.com $argv!!" | searchlink)
+    end
+
 end
